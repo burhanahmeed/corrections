3f04a9b4808a8e53dd5cc9ac931c848338122080
burhanahmeed <burhanahmeed@gmail.com>
2021-03-25 21:10:58 +0700

    fix gridjs customization

--- frontend/static/js/users-view.js
+++ frontend/static/js/users-view.js
@@ -1,31 +1,86 @@
 import Vue from 'https://unpkg.com/vue@2.6.0/dist/vue.esm.browser.min.js';
 import {h, Grid, PluginPosition} from 'https://cdn.skypack.dev/gridjs';
 
-function MyPlugin() {
-  return h('a', {
-    class: 'btn btn-primary',
-    style: 'position: absolute; right: 10px;',
-    id: "add-button"
-  }, 'Add user');
-}
-
 Vue.component('users-view', {
   data: function () {
     return {
-      hello: 'hellow'
+      gridInstance: null,
+      gridOptions: {
+        columns: [
+          'Username', 
+          'Permissions',
+          {
+            name: 'Action',
+            formatter: (cell, row) => gridjs.html(
+              `<a class="badge badge-secondary" href="/dashboard/edit/${row.cells[0].data}">Edit</a> | <form id="form_${row.cells[0].data}" class="d-inline-block" method="post" action="/api/dictionary/delete/${row.cells[0].data}"></form><button class="badge badge-danger pointer border-0" onClick="handleDelete(${row.cells[0].data})" type="button">Delete</button>`
+            )
+          }
+        ],
+        server: {
+          url: 'https://my-json-server.typicode.com/burhanahmeed/sunnah-com-mock/users',
+          then: data => data.map(user => [
+              user.username,
+              user.permissions
+            ]
+          )
+        },
+        search: {
+          enabled: true
+        },
+        pagination: {
+          buttonsCount: 10,
+          limit: 10,
+          summary: true,
+        },
+      },
+      users: [],
     }
   },
   methods: {
-    click () {
-      alert('hello')
+    onInit () {
+      return new Promise(resolve => {
+        fetch('https://my-json-server.typicode.com/burhanahmeed/sunnah-com-mock/users', {
+          method: 'GET'
+        })
+        .then(response => response.json())
+        .then(response => resolve(response))
+        const vm = this;
+
+        const tableHead = document.getElementsByClassName('gridjs-head');
+        if (tableHead) {
+          tableHead[0].innerHTML = `
+            <a id="add-button" class="btn btn-primary" style="position: absolute; right: 10px;">
+              Add user
+            </a>
+          `;
+        }
+
+        document.getElementById('add-button').addEventListener('click', () => {
+          vm.click();
+        })
+      })
+    },
+    async click () {
+      this.gridInstance.updateConfig({
+        data: () => this.onInit()
+      })
+      this.gridInstance.forceRender();
     }
   },
   mounted: function () {
-    const gridjsInit = new gridjs.Grid({
+    this.gridInstance = new Grid({
       columns: [
-        'First name', 
-        'Last name', 
-        'Role (permission)', 
+        {
+          id: 'username',
+          name: 'Username'
+        },
+        {
+          id: 'permissions',
+          name: 'Permissions',
+          formatter: (cell, row) => gridjs.html(
+            `tes: ${JSON.stringify(row.cells[1].data)}`
+          )
+        },
         {
           name: 'Action',
           formatter: (cell, row) => gridjs.html(
@@ -33,38 +88,18 @@ Vue.component('users-view', {
           )
         }
       ],
-      server: {
-        url: 'https://my-json-server.typicode.com/burhanahmeed/sunnah-com-mock/users',
-        then: data => data.map(user => [
-            user.fname,
-            user.lname,
-            user.role,
-            ''
-          ]
-        )
-      },
+      data: () => this.onInit(),
       search: {
-        enabled: true
+        enabled: true,
       },
       pagination: {
         buttonsCount: 10,
-        limit: 15,
-        summary: true
+        limit: 10,
+        summary: true,
       },
-    });
-    gridjsInit.plugin.add({
-      id: 'myplugin',
-      component: MyPlugin,
-      position: PluginPosition.Header,
     });
 
     // render gridjs to html
-    gridjsInit.render(document.getElementById("table"));
-
-    const vm = this;
-
-    document.getElementById('add-button').addEventListener('click', () => {
-      vm.click();
-    })
+    this.gridInstance.render(document.getElementById("table"));
   }
 })
