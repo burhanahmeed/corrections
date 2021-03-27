6748c4cb769731dbd16cc0a91a752f754b2714f9
burhanahmeed <burhanahmeed@gmail.com>
2021-03-27 21:46:56 +0700

    tmp commit

--- frontend/static/js/users-view.js
+++ frontend/static/js/users-view.js
@@ -10,34 +10,6 @@ Vue.component('users-view', {
   data: function () {
     return {
       gridInstance: null,
-      gridOptions: {
-        columns: [
-          'Username', 
-          'Permissions',
-          {
-            name: 'Action',
-            formatter: (cell, row) => gridjs.html(
-              `<a class="badge badge-secondary" href="/dashboard/edit/${row.cells[0].data}">Edit</a> | <form id="form_${row.cells[0].data}" class="d-inline-block" method="post" action="/api/dictionary/delete/${row.cells[0].data}"></form><button class="badge badge-danger pointer border-0" onClick="handleDelete(${row.cells[0].data})" type="button">Delete</button>`
-            )
-          }
-        ],
-        server: {
-          url: 'https://my-json-server.typicode.com/burhanahmeed/sunnah-com-mock/users',
-          then: data => data.map(user => [
-              user.username,
-              user.permissions
-            ]
-          )
-        },
-        search: {
-          enabled: true
-        },
-        pagination: {
-          buttonsCount: 10,
-          limit: 10,
-          summary: true,
-        },
-      },
       users: [],
     }
   },
@@ -48,61 +20,83 @@ Vue.component('users-view', {
           method: 'GET'
         })
         .then(response => response.json())
-        .then(response => resolve(response))
-        const vm = this;
-
-        const tableHead = document.getElementsByClassName('gridjs-head');
-        if (tableHead) {
-          tableHead[0].innerHTML = `
-            <a id="add-button" class="btn btn-primary" style="position: absolute; right: 10px;">
-              Add user
-            </a>
-          `;
-        }
-
-        document.getElementById('add-button').addEventListener('click', () => {
-          vm.handleAddUserModal();
+        .then(response => {
+          console.log(response);
+          this.users = response;
+          // this.reloadData();
+          resolve(response);
         })
+      })
+    },
+    reloadButton () {
+      const vm = this;
+
+      const tableHead = document.getElementsByClassName('gridjs-head');
+      if (tableHead) {
+        tableHead[0].innerHTML = `
+          <a id="add-button" class="btn btn-primary" style="position: absolute; right: 10px;">
+            Add user
+          </a>
+        `;
+      }
+
+      document.getElementById('add-button').addEventListener('click', () => {
+        vm.handleAddUserModal();
       })
     },
     reloadData () {
       this.gridInstance.updateConfig({
-        data: () => this.onInit()
+        // data: () => this.onInit()
+        from: document.getElementById("tableContent"),
       })
       this.gridInstance.forceRender();
+      this.reloadButton();
     },
     handleAddUserModal () {
       this.$vm2.open('add-user');
     },
     closeModal () {
       this.$vm2.close('add-user');
+    },
+    handleDelete () {
+      if (confirm('Are you sure want to delete this user?')) {
+        console.log('deleted!');
+      }
     }
   },
-  mounted: function () {
+  mounted: async function () {
+    await this.onInit();
     this.gridInstance = new Grid({
-      columns: [
-        {
-          id: 'username',
-          name: 'Username'
-        },
-        {
-          id: 'permissions',
-          name: 'Permissions',
-          formatter: (cell, row) => gridjs.html(
-            <span></span>
-          )
-        },
-        {
-          name: 'Action',
-          formatter: (cell, row) => gridjs.html(
-            `<a class="badge badge-secondary" href="/dashboard/edit/${row.cells[0].data}">Edit</a> | <form id="form_${row.cells[0].data}" class="d-inline-block" method="post" action="/api/dictionary/delete/${row.cells[0].data}"></form><button class="badge badge-danger pointer border-0" onClick="handleDelete(${row.cells[0].data})" type="button">Delete</button>`
-          )
-        }
-      ],
-      data: () => this.onInit(),
+      // columns: [
+      //   {
+      //     id: 'username',
+      //     name: 'Username'
+      //   },
+      //   {
+      //     id: 'permissions',
+      //     name: 'Permissions',
+      //     formatter: (cell, row) => gridjs.html(
+            
+      //     )
+      //   },
+      //   {
+      //     name: 'Action',
+      //     formatter: (cell, row) => gridjs.html(
+      //       `<a href="/dashboard/edit/${row.cells[0].data}">
+      //         <i class="far fa-edit"></i>
+      //       </a>
+      //       \u00A0
+      //       <a class="text-danger" href="/dashboard/edit/${row.cells[0].data}">
+      //         <i class="far fa-trash-alt"></i>
+      //       </a>`
+      //     )
+      //   }
+      // ],
+      // data: () => this.onInit(),
       search: {
         enabled: true,
       },
+      from: document.getElementById("tableContent"),
       pagination: {
         buttonsCount: 10,
         limit: 10,
@@ -112,5 +106,9 @@ Vue.component('users-view', {
 
     // render gridjs to html
     this.gridInstance.render(document.getElementById("table"));
+    
+    setTimeout(() => {
+      this.reloadData();  
+    }, 1000);
   }
 })
